b52d0a758b88abc8a6803b83ca71194ab77e8d8c
deqan <andrey.rutko@mail.ru>
2023-07-26 21:49:37 +0300

    9 zadacha

--- first_project/bin/zadchi/zadacha_9.dart
+++ first_project/bin/zadchi/zadacha_9.dart
@@ -1,2 +1,13 @@
-//21
-void main() {}
+void main() {
+  int a = 1, b = 9, c = 6, x = 0;
+  int prog = b - a;
+  c = c - 1;
+  print("Прогрессия прибавляется на $prog");
+  print("$a");
+  do {
+    a = a + prog;
+    x++;
+    print("$a");
+  } while (x < c);
+  print("Результат: $a");
+}
