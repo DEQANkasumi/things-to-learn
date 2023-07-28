437c8c922d3463637138eaf0afe9cfef3af825de
deqan <andrey.rutko@mail.ru>
2023-07-28 10:59:00 +0300

    10 zadacha

--- first_project/bin/zadchi/zadacha_10.dart
+++ first_project/bin/zadchi/zadacha_10.dart
@@ -1,8 +1,23 @@
 // 331
 void main() {
-  final depTime = DateTime(00, 00);
-  final wayTime = DateTime(10, 10);
+  String hourStr = "11"; // Час отправления.
+  String minStr = "00"; // Минута отправления.
+  int wayTimeHour = 22; // Часов в пути.
+  int wayTimeMin = 0; //  Минут в пути.
 
-  final finalTime = wayTime;
-  print("$finalTime");
+  int hour = int.parse(hourStr);
+  int min = int.parse(minStr);
+  print("Время отправления: $hourStr:$minStr");
+
+  int arrivalTimeHour = hour + wayTimeHour;
+  if (arrivalTimeHour > 24) arrivalTimeHour = arrivalTimeHour - 24;
+  int arrivalTimeMin = min + wayTimeMin;
+  if (arrivalTimeMin > 60) arrivalTimeMin = arrivalTimeMin - 60;
+
+  String arrivalTimeHourStr = arrivalTimeHour.toString();
+  String addNull = ("0");
+  if (arrivalTimeHourStr.length < 2) arrivalTimeHourStr = addNull + arrivalTimeHourStr;
+  String arrivalTimeMinStr = arrivalTimeMin.toString();
+  if (arrivalTimeMinStr.length < 2) arrivalTimeMinStr = addNull + arrivalTimeMinStr;
+  print("Время прибытия: $arrivalTimeHourStr:$arrivalTimeMinStr");
 }
